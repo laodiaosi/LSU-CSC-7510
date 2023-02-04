a0c2eda20edd1882f1c87a3131906e8abbc4224a
laodiaosi <37257610+laodiaosi@users.noreply.github.com>
2023-02-04 01:51:45 +0000

    Add files via upload

--- assignment/assignment3/ImageServer.java
+++ assignment/assignment3/ImageServer.java
@@ -1,6 +1,3 @@
-package asign275101;
-//package socket.test1;
-
 
 import java.io.*;
 import java.net.*;
@@ -19,22 +16,32 @@ public class ImageServer {
             DataOutputStream  out = new DataOutputStream (s.getOutputStream());  
            
             //get picture name
-            String str,str1;
-            byte[] buffer = new byte[1024];
+            //input filename version
+//            InputStream input =s.getInputStream();
+//            BufferedReader reader = new BufferedReader(new InputStreamReader(input));
+//            String filename = reader.readLine();
+            String str1;
+            String fileName = "Koala.jpg";
+            byte[] buffer = new byte[4096];
 			/* Detailed requirement below*/	
 			//Step one: check the picture name sent from the client, if the picture name equals "Koala.jpg", go to step two, otherwise go to step three
-            if ((str = in.readLine()) == "Koala.jpg") {
+            String str;
+			if ((str = in.readLine()).equals(fileName)) {
                 //Step two, read the picture "Koala.jpg" from the local disk, and send the content back to the client. 
-            	FileInputStream getFileStream = new FileInputStream(str);
-            	int count;
-            	while((count = getFileStream.read(buffer)) > 0) {
-            		out.write(buffer,0,count);
+            	File file = new File(fileName);
+            	FileInputStream getFileStream = new FileInputStream(file);
+            	int len = -1;
+            	while((len = getFileStream.read(buffer)) > 0) {
+            		out.write(buffer,0,len);
             	}
+            	out.flush();
             	getFileStream.close();
-            }else {
+            }else{
                 //step three, then reply to the client with "Sorry, no such picture",
             	str1 = "Sorry, no such picture";
                 out.writeBytes(str1);
+//                out.write();
+                out.flush();
             }
             
             //step four, close the input/output streams, close the socket.
@@ -42,5 +49,7 @@ public class ImageServer {
             out.close();
             s.close();
          }
+
        } catch (Exception e) { e.printStackTrace(); }
    }
+}
